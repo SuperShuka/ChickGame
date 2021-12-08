267a39a17ff4217dafddf7af2f167ddbbf146bc1
Sanya <57352066+SuperShuka@users.noreply.github.com>
2021-12-08 18:02:48 +0300

    f

--- main.py
+++ main.py
@@ -7,26 +7,39 @@ sc = pygame.display.set_mode((W, H))
 pygame.display.set_caption('Chick Game')
 pygame.display.set_icon(pygame.image.load('Assets/Images/chickright.png'))
 
+chickleft = pygame.image.load('Assets/Images/chickleft.png')
+chickright = pygame.image.load('Assets/Images/chickright.png')
+chickleft = pygame.image.load('Assets/Images/chickjumpleft.png')
+chickright = pygame.image.load('Assets/Images/chickright.png')
+
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 BLACK = (0, 0, 0)
 
+ground = 50
+yspeed = 0
+xspeed = 0
 jump_height = 50
+
+you =
+
+
+def move():
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_SPACE] and ground==:
+
+
 
 FPS = 90
 clock = pygame.time.Clock()
 
 while True:
-    keys = pygame.key.get_pressed()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
         if event.type == pygame.K_ESCAPE:
             exit()
-
-    sc.fill(BLACK)
-    pygame.display.update()
 
     clock.tick(FPS)
